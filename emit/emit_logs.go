ef9e844ebdf03016a8bccf24219275603ca598a1
Francesco Torchia <francesco.torchia@suse.com>
2023-11-05 18:54:44 +0100

    fix go code spacing
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- emit/emit_logs.go
+++ emit/emit_logs.go
@@ -1,14 +1,14 @@
 package main
 
 import (
- "context"
- "fmt"
- "log"
- "os"
- "strings"
- "time"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
 
- amqp "github.com/rabbitmq/amqp091-go"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 var service = os.Getenv("rmq_service")
@@ -19,61 +19,61 @@ var password = os.Getenv("rmq_password")
 var queue = os.Getenv("rmq_queue")
 
 func failOnError(err error, msg string) {
- if err != nil {
-  log.Panicf("%s: %s", msg, err)
- }
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
 }
 
 func main() {
- address := fmt.Sprintf("amqp://%s:%s@%s:%s/", username, password, service, port)
+	address := fmt.Sprintf("amqp://%s:%s@%s:%s/", username, password, service, port)
 
- conn, err := amqp.Dial(address)
- failOnError(err, "Failed to connect to RabbitMQ")
- defer conn.Close()
+	conn, err := amqp.Dial(address)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
 
- ch, err := conn.Channel()
- failOnError(err, "Failed to open a channel")
- defer ch.Close()
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
 
- err = ch.ExchangeDeclare(
-  "logs_topic", // name
-  "topic",      // type
-  true,         // durable
-  false,        // auto-deleted
-  false,        // internal
-  false,        // no-wait
-  nil,          // arguments
- )
- failOnError(err, "Failed to declare an exchange")
+	err = ch.ExchangeDeclare(
+		"logs_topic", // name
+		"topic",      // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
 
- ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
- defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
- body := "ciao"
+	body := "ciao"
 
- for {
-  err = ch.PublishWithContext(ctx,
-   "logs_topic", // exchange
-   queue,        // routing key
-   false,        // mandatory
-   false,        // immediate
-   amqp.Publishing{
-    ContentType: "text/plain",
-    Body:        []byte(body),
-   })
-  failOnError(err, "Failed to publish a message")
+	for {
+		err = ch.PublishWithContext(ctx,
+			"logs_topic", // exchange
+			queue,        // routing key
+			false,        // mandatory
+			false,        // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		failOnError(err, "Failed to publish a message")
 
-  log.Printf(" [x] Sent %s", body)
-  time.Sleep(5 * time.Second)
- }
+		log.Printf(" [x] Sent %s", body)
+		time.Sleep(5 * time.Second)
+	}
 }
 
 func bodyFrom(args []string) string {
- var s string
- if (len(args) < 3) || os.Args[2] == "" {
-  s = "hello"
- } else {
-  s = strings.Join(args[2:], " ")
- }
- return s
+	var s string
+	if (len(args) < 3) || os.Args[2] == "" {
+		s = "hello"
+	} else {
+		s = strings.Join(args[2:], " ")
+	}
+	return s
 }
